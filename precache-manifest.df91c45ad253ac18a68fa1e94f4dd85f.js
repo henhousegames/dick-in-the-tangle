bc52c81cafef6edb5550777e8cb6f8b537f42a01
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-13 01:54:13 +0000

    Deploy henhousegames/dick-in-the-tangle to github.com/henhousegames/dick-in-the-tangle.git:gh-pages

---
+++ precache-manifest.df91c45ad253ac18a68fa1e94f4dd85f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbbc7c1dda8c14309c9f5f4b98206bd7",
+    "url": "assets/img/dick.png"
+  },
+  {
+    "revision": "b887c333d5ec8274b78d0851e7b3337b",
+    "url": "assets/img/phaser-logo.png"
+  },
+  {
+    "revision": "57040e5677322118f6d56a1d9e43c5c6",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "2ffbc23293ee8a797bc61e9c02534206",
+    "url": "icons/icons-192.png"
+  },
+  {
+    "revision": "8bdcc486cda9b423f50e886f2ddb6604",
+    "url": "icons/icons-512.png"
+  },
+  {
+    "revision": "169fa450cfaf2f4cd05f96b218c7cc7c",
+    "url": "index.html"
+  },
+  {
+    "revision": "a2834937401c38baf7c1",
+    "url": "main.0c16836eb18eb6fe49e3.bundle.js"
+  },
+  {
+    "revision": "bf38d0c9760162342f12cd9b44fbf4ef",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "sw.js"
+  },
+  {
+    "revision": "615440a368f3299a4f39",
+    "url": "vendors.e8e9883958236596cd8b.bundle.js"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,11 @@
+importScripts("precache-manifest.df91c45ad253ac18a68fa1e94f4dd85f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
